ff8039cb5169e6f7446440cae2c204cf0a0b3f65
Robert (Robby) O'Connor <robby.oconnor@gmail.com>
2014-05-27 02:27:21 -0400

    Update PiClock.py

--- PiClock.py
+++ PiClock.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python
 
 from time import sleep
-import datetime
-import random 
+import datetime,random
 from Adafruit_CharLCDPlate import Adafruit_CharLCDPlate
 
 # Initialize the LCD plate.  Should auto-detect correct I2C bus.  If not,
 # pass '0' for early 256 MB Model B boards or '1' for all later versions
 lcd = Adafruit_CharLCDPlate(busnum = 0)
-r = random.random()
 
 # Cycle through backlight colors
 col = (lcd.RED , lcd.YELLOW, lcd.GREEN, lcd.TEAL,
